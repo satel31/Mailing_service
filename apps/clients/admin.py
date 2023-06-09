c3a00241ac072c75bf7f087b0fdf34065e114138
satel31 <blokhnina.tatiana@yandex.ru>
2023-06-09 23:11:16 +0300

    Added admin view

--- apps/clients/admin.py
+++ apps/clients/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from apps.clients.models import Client
 
-# Register your models here.
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'email', 'first_name', 'last_name', 'comment',)
+    list_filter = ('comment',)
+    search_fields = ('email', 'first_name', 'last_name',)
+
+
+
