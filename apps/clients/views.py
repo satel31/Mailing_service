2e376e3f2615f61a3b3027c2d1a7d8821c0eef21
satel31 <blokhnina.tatiana@yandex.ru>
2023-06-10 01:09:41 +0300

    Added CreateView and ListView

--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import CreateView, ListView
 
-# Create your views here.
+from apps.clients.models import Client
+
+
+# CRUD
+class ClientCreateView(CreateView):
+    model = Client
+    fields = ('email', 'first_name', 'last_name', 'comment',)
+    #success_url = reverse_lazy('clients: all_clients')
+
+class ClientListView(ListView):
+    model = Client
+    extra_context = {
+        'title': 'Your Client List',
+    }
+
+
