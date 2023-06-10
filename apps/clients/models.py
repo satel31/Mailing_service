d3d489d22a67871bade4083ea25d16f0b7c639f8
satel31 <blokhnina.tatiana@yandex.ru>
2023-06-11 00:20:42 +0300

    Deleted last_name, made name

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -3,14 +3,13 @@ from django.db import models
 NULLABLE = {'blank': True, 'null': True}
 
 
-class Client(models.Model):
+class Clients(models.Model):
     email = models.EmailField(unique=True, verbose_name='email')
-    first_name = models.CharField(max_length=250, verbose_name='first_name')
-    last_name = models.CharField(max_length=250, verbose_name='last_name', **NULLABLE)
+    name = models.CharField(max_length=250, verbose_name='first_name')
     comment = models.TextField(**NULLABLE)
 
     def __str__(self):
-        return f'{self.email} {self.first_name}'
+        return f'{self.email} {self.name}'
 
     class Meta:
         verbose_name = 'client'
