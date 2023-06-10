f0f81c0e45d377984e011ebc0d66990b9ad650a7
satel31 <blokhnina.tatiana@yandex.ru>
2023-06-11 00:21:06 +0300

    Added views

--- apps/clients/views.py
+++ apps/clients/views.py
@@ -1,18 +1,39 @@
-from django.views.generic import CreateView, ListView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, ListView, UpdateView, DeleteView
 
-from apps.clients.models import Client
+from apps.clients.models import Clients
 
 
 # CRUD
-class ClientCreateView(CreateView):
-    model = Client
-    fields = ('email', 'first_name', 'last_name', 'comment',)
-    #success_url = reverse_lazy('clients: all_clients')
+class ClientsCreateView(CreateView):
+    model = Clients
+    fields = ('email', 'name', 'comment',)
+    success_url = reverse_lazy('clients:clients_list')
 
-class ClientListView(ListView):
-    model = Client
+class ClientsListView(ListView):
+    model = Clients
     extra_context = {
         'title': 'Your Client List',
     }
+
+    def get_queryset(self):
+        return super().get_queryset().order_by('pk')
+
+class ClientsDetailView(DeleteView):
+    model = Clients
+    template_name = 'clients/clients_detail.html'
+
+    def get_context_data(self, *args, **kwargs):
+        context_data = super().get_context_data(**kwargs)
+        context_data['title'] = context_data['object'].name
+        return context_data
+class ClientsUpdateView(UpdateView):
+    model = Clients
+    fields = ('email', 'name', 'comment',)
+    success_url = reverse_lazy('clients:clients_list')
+
+class ClientsDeleteView(DeleteView):
+    model = Clients
+    success_url = reverse_lazy('clients:clients_list')
 
 
