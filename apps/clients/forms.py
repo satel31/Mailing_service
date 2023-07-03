065ec87fc49da4af855fd98842ddefba9ecd5e5d
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-03 22:16:19 +0300

    Made forms

---
+++ apps/clients/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from apps.clients.models import Clients, Groups
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control-10'
+
+
+class ClientsForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Clients
+        fields = '__all__'
+
+
+class GroupsForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Groups
+        fields = '__all__'
