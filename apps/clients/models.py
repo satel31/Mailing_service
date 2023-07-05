ff2f9df5f83c621c210ab86efb01c099e211913e
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-06 00:38:58 +0300

    Created models for mailings

--- apps/clients/models.py
+++ apps/clients/models.py
@@ -2,21 +2,6 @@ from django.db import models
 
 NULLABLE = {'blank': True, 'null': True}
 
-
-class Clients(models.Model):
-    email = models.EmailField(unique=True, verbose_name='email')
-    name = models.CharField(max_length=250, verbose_name='name')
-    comment = models.TextField(**NULLABLE)
-    group = models.ForeignKey('Groups', on_delete=models.PROTECT, verbose_name='Group')
-
-    def __str__(self):
-        return f'{self.email} {self.name}'
-
-    class Meta:
-        verbose_name = 'client'
-        verbose_name_plural = 'clients'
-
-
 class Groups(models.Model):
     group_name = models.CharField(max_length=250, verbose_name='Group Name')
     description = models.TextField(verbose_name='Description', **NULLABLE)
@@ -27,3 +12,16 @@ class Groups(models.Model):
     class Meta:
         verbose_name = 'group'
         verbose_name_plural = 'groups'
+
+class Clients(models.Model):
+    email = models.EmailField(unique=True, verbose_name='Email')
+    name = models.CharField(max_length=250, verbose_name='Name')
+    comment = models.TextField(**NULLABLE)
+    group = models.ForeignKey(Groups, on_delete=models.PROTECT, verbose_name='Group')
+
+    def __str__(self):
+        return f'{self.email} {self.name}'
+
+    class Meta:
+        verbose_name = 'client'
+        verbose_name_plural = 'clients'
