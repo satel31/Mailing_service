8952a31bb9c9eab712ecf5254f0e46d7c400ecd1
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-06 20:13:35 +0300

    Made forms mailings

---
+++ apps/mailings/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+
+from apps.mailings.models import Mail, Mailings
+
+
+class StyleFormMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control-10'
+
+
+class MailForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Mail
+        fields = '__all__'
+
+
+class MailingsForm(StyleFormMixin, forms.ModelForm):
+    class Meta:
+        model = Mailings
+        fields = '__all__'
+        exclude = ('status',)
