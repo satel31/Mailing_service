0d38b973c7ee1a846ab151b590d9cd76668fb3ac
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-07 21:41:08 +0300

    Added logs creating and try/except

--- apps/mailings/services.py
+++ apps/mailings/services.py
@@ -5,21 +5,37 @@ import schedule
 from django.core.mail import send_mail
 from django.utils import timezone
 from apps.clients.models import Clients
-from apps.mailings.models import Mailings
+from apps.mailings.models import Mailings, MailingLog
 from config import settings
 
 
 def send_email(obj):
+    status = []
     clients = Clients.objects.filter(group=obj.clients.pk)
     for client in clients:
-        send_mail(
-            obj.text.subject,
-            obj.text.body,
-            settings.EMAIL_HOST_USER,
-            [client.email]
-        )
+        try:
+            send_mail(
+                obj.text.subject,
+                obj.text.body,
+                settings.EMAIL_HOST_USER,
+                [client.email]
+            )
+        except Exception as e:
+            server_response = {
+                'attempt_status': 'failure',
+                'response': f'Error:{str(e)}',
+                'mailing': obj,
+            }
+            status.append(MailingLog(**server_response))
+        else:
+            server_response = {
+                'attempt_status': 'success',
+                'response': 'E-mail was sent successfully',
+                'mailing': obj,
+            }
+            status.append(MailingLog(**server_response))
 
-
+    MailingLog.objects.bulk_create(status)
 def send_mailing():
     """If start time <= now <= end time, sends email"""
     mailings = Mailings.objects.all()
@@ -33,6 +49,7 @@ def send_mailing():
                 mailing.time += timedelta(days=7)
             elif mailing.frequency == 'M':
                 mailing.time += timedelta(days=30)
+
 
 def run_schedule():
     schedule.every(60).seconds.do(send_mailing)
