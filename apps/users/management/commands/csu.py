c2dface5fb8e4d10cfefc4784a309bd79d0221b7
satel31 <blokhnina.tatiana@yandex.ru>
2023-07-07 23:19:28 +0300

    Created command for superuser

---
+++ apps/users/management/commands/csu.py
@@ -0,0 +1,17 @@
+from django.core.management import BaseCommand
+
+from apps.users.models import User
+
+
+class Command(BaseCommand):
+    def handle(self, *args, **options):
+        user = User.objects.create(
+            email='admin@sky.pro',
+            first_name='Admin',
+            last_name='SkyPro',
+            is_staff=True,
+            is_superuser=True,
+        )
+
+        user.set_password('123456')
+        user.save()
